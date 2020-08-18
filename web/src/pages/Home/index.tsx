164cfdbdc6d45ade511ae47d337d4f05de9a12b4
Gilson da Gama <gilsondagama@hotmail.com>
2020-08-18 03:19:18 -0300

    web: Web application created

--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -1,33 +1,33 @@
 import React from 'react';
-// import { FiLogIn } from 'react-icons/fi'
-// import { Link } from 'react-router-dom'
+import { FiLogIn } from 'react-icons/fi'
+import { Link } from 'react-router-dom'
 
-// import logo from '../../assets/logo.svg'
+import logo from '../../assets/logo.svg'
 
-// import './styles.css';
+import './styles.css'
 
 const Home = () => {
   return (
     <div id="page-home">
       <div className="content">
-        <header>{/* <img src={logo} alt="Logo"/> */}</header>
+        <header>
+          <img src={logo} alt="Logo"/>
+        </header>
 
         <main>
           <h1>Seu marketplace de coleta de resíduos.</h1>
-          <p>
-            Ajudamos pessoas a encontrarem pontos de coleta de forma diferente.
-          </p>
+          <p>Ajudamos pessoas a encontrarem pontos de coleta de forma diferente.</p>
 
-          {/* <Link to="/create-point">
+          <Link to="/create-point">
             <span>
               <FiLogIn />
             </span>
             <strong>Cadastre um ponto de coleta</strong>
-          </Link> */}
+          </Link>
         </main>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
